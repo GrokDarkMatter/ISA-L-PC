29d01cde0762107bbdd8732f9d45c052fa50e1bc
GrokDarkMatter <andinc@aol.com>
2025-10-07 13:59:36 +0700

    Added check_gfni_support function to PC_CPU_ID

--- erasure_code/PC_CPU_ID.c
+++ erasure_code/PC_CPU_ID.c
@@ -4,66 +4,104 @@
 #include <windows.h>
 #include <intrin.h>
 
-void get_cpu_brand(char* brand) {
-    int regs[4];
-    char* p = brand;
+void
+get_cpu_brand (char *brand)
+{
+    int regs[ 4 ];
+    char *p = brand;
     unsigned int max_level;
 
-    __cpuid(regs, 0x80000000);
-    max_level = regs[0];
+    __cpuid (regs, 0x80000000);
+    max_level = regs[ 0 ];
 
-    if (max_level < 0x80000004) {
-        //strcpy(brand, "Unknown");
+    if (max_level < 0x80000004)
+    {
+        strcpy (brand, "Unknown");
         return;
     }
 
-    for (unsigned int i = 0x80000002; i <= 0x80000004; ++i) {
-        __cpuid(regs, i);
-        memcpy(p, regs, sizeof(regs));
-        p += sizeof(regs);
+    for (unsigned int i = 0x80000002; i <= 0x80000004; ++i)
+    {
+        __cpuid (regs, i);
+        memcpy (p, regs, sizeof (regs));
+        p += sizeof (regs);
     }
     *p = '\0';
 
     // Trim leading/trailing spaces
-    p = brand + strlen(brand) - 1;
-    while (p > brand && *p == ' ') *p-- = '\0';
-    while (*brand == ' ') ++brand;
+    p = brand + strlen (brand) - 1;
+    while (p > brand && *p == ' ')
+        *p-- = '\0';
+    while (*brand == ' ')
+        ++brand;
 }
 
-int PC_CPU_ID() {
-    char brand[64];
-    get_cpu_brand(brand);
+int
+check_gfni_support ()
+{
+    int regs[ 4 ];
+    __cpuid (regs, 0x7);                // Leaf 0x7, subleaf 0
+    return (regs[ 2 ] & (1 << 8)) != 0; // Check ECX bit 8 for GFNI
+}
+
+int
+PC_CPU_ID ()
+{
+    char brand[ 64 ] = { 0 }; // Initialize to avoid garbage
+    get_cpu_brand (brand);
 
     SYSTEM_INFO si;
-    GetSystemInfo(&si);
+    GetSystemInfo (&si);
 
-    printf("Processor Brand: %s\n", brand);
-    printf("Number of Logical Processors: %u\n", si.dwNumberOfProcessors);
-    printf("Processor Architecture: ");
-    switch (si.wProcessorArchitecture) {
-    case PROCESSOR_ARCHITECTURE_AMD64: printf("x64\n"); break;
-    case PROCESSOR_ARCHITECTURE_INTEL: printf("x86\n"); break;
-    case PROCESSOR_ARCHITECTURE_ARM: printf("ARM\n"); break;
-    default: printf("Unknown\n");
+    printf ("Processor Brand: %s\n", brand);
+    printf ("Number of Logical Processors: %u\n", si.dwNumberOfProcessors);
+    printf ("Processor Architecture: ");
+    switch (si.wProcessorArchitecture)
+    {
+    case PROCESSOR_ARCHITECTURE_AMD64:
+        printf ("x64\n");
+        break;
+    case PROCESSOR_ARCHITECTURE_INTEL:
+        printf ("x86\n");
+        break;
+    case PROCESSOR_ARCHITECTURE_ARM:
+        printf ("ARM\n");
+        break;
+    default:
+        printf ("Unknown\n");
+        break;
     }
+
+    printf ("GFNI Support: %s\n", check_gfni_support () ? "Yes" : "No");
 
     HKEY hKey;
     DWORD mhz = 0;
-    DWORD size = sizeof(DWORD);
-    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
-        RegQueryValueEx(hKey, "~MHz", NULL, NULL, (LPBYTE)&mhz, &size); // Use LPBYTE
-        RegCloseKey(hKey);
-        printf("Approximate Clock Speed: %u MHz\n", mhz);
+    DWORD size = sizeof (DWORD);
+    if (RegOpenKeyEx (HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0", 0,
+                      KEY_READ, &hKey) == ERROR_SUCCESS)
+    {
+        RegQueryValueEx (hKey, "~MHz", NULL, NULL, (LPBYTE) &mhz, &size);
+        RegCloseKey (hKey);
+        printf ("Approximate Clock Speed: %u MHz\n", mhz);
     }
-    else {
-        //printf("Failed to retrieve clock speed from registry\n");
+    else
+    {
+        printf ("Failed to retrieve clock speed from registry\n");
     }
 
-    return si.dwNumberOfProcessors;
+    return 0;
 }
 #else
 #include <stdio.h>
 #include <string.h>
+
+int
+check_gfni_support ()
+{
+    unsigned int eax, ebx, ecx, edx;
+    __cpuid (0x7, eax, ebx, ecx, edx); // Leaf 0x7, subleaf 0
+    return (ecx & (1 << 8)) != 0;      // Check ECX bit 8 for GFNI
+}
 
 int PC_CPU_ID( void ) {
     FILE* fp = fopen("/proc/cpuinfo", "r");
@@ -72,6 +110,11 @@ int PC_CPU_ID( void ) {
         return 1;
     }
 
+int check_gfni_support() {
+    unsigned int eax, ebx, ecx, edx;
+    __cpuid(0x7, eax, ebx, ecx, edx); // Leaf 0x7, subleaf 0
+    return (ecx & (1 << 8)) != 0; // Check ECX bit 8 for GFNI
+}
     int s=0, Cores = 0, m ;
     char line[1024];
     printf("Processor Details:\n");
@@ -104,7 +147,7 @@ int PC_CPU_ID( void ) {
             {
                 strncpy ( line, "CPU part	: Cortex-A76 core\n", 28 ) ; ;
             } 
-            m = 1 ;
+            m = 1;
         }
         if (strncmp(line, "CPU revision", 12) == 0) m = 1 ;
         if (strncmp(line, "BogoMIPS", 8) == 0) m = 1 ;
@@ -124,6 +167,7 @@ int PC_CPU_ID( void ) {
         }
     }
     printf ( "cpu cores	: %d\n", Cores ) ;
+    printf ("GFNI Support: %s\n", check_gfni_support () ? "Yes" : "No");
 
     fclose(fp);
     return Cores;
